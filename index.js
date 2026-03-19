de8bb0e1b19bd2e70501f26eb54addb10ead8d3d
VicenteZanatta <vicentezanatta03@gmail.com>
2026-03-19 16:48:01 -0300

    Creat fetchAUXprices() function that uses XAU prices public API

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+require('dotenv').config();
+
 const express = require("express");
 const fs = require('fs');
-const app = express()
+
+const app = express();
+
+const alphaVantageKey = process.env.ZLW_ALPHAVANTAGE_PUBLICAPI_KEY;
 
 app.get('/', (req, res) => {
-
     fs.readFile('./home.html', 'utf8', (err, html) => {
-
         if (err){
-            res.status(500).send('sorry, out of order')
+            return res.status(500).send('sorry, out of order');
         }
-
-        res.send(html)
-    })
+        res.send(html);
+    });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('App avaiable on http://localhost:3000'))
+async function fetchAUXprocies() {
+    const url = `https://www.alphavantage.co/query?function=GOLD_SILVER_SPOT&symbol=XAU&apikey=${alphaVantageKey}`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            console.log('Status:', response.status);
+            return;
+        }
+
+        const data = await response.json();
+        console.log(data);
+
+    } catch (err) {
+        console.log('Error:', err);
+    }
+}
+
+app.listen(process.env.PORT, () => 
+    console.log(`App available on http://localhost:${process.env.PORT}`)
+);
+
+fetchAUXprocies();
